3603aa049a5a95ef1f0967a063e5ed1ab134ecc7
sergio ivan lopez monzon <sergioivan154@gmail.com>
2018-09-10 18:51:47 -0500

    para subir cosas a s3

--- api/controllers/EstudianteController.js
+++ api/controllers/EstudianteController.js
@@ -8,6 +8,7 @@ module.exports = {
 		new:function (req, res){
 		res.view()
 	},
+	
 	create:function(req, res){
 		var EstudianteObj={
 			nombre:req.param('txtnombre')
@@ -19,7 +20,35 @@ module.exports = {
 			}
 				res.redirect('/estudiante');
 		});
-	}
+	},
+
+
+
+	uploadFile: function (req, res) {
+
+		const options =
+      	{ // This is the usual stuff
+        	adapter: require('skipper-s3'),
+	      		key: 'AKIAJYOH6EKNWFPXMCFQ',
+		      	secret: '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',
+		      	bucket: 'pruebasails',
+		      	region: 'us-east-2',
+	      	s3params:
+	        	{ 
+	        		ACL: 'public-read'
+	        	},
+      		onProgress: progress => sails.log.verbose('Upload progress:', progress)
+     	}
+
+		req.file('avatar').upload(options, (err, uploadedFiles) => {
+		   if (err) return res.send(500, err);
+				  return res.json({
+				    message: uploadedFiles.length + ' file(s) uploaded successfully!',
+				    files: uploadedFiles
+				  });
+		})
+  	
+  	}
 	
 	
 };
