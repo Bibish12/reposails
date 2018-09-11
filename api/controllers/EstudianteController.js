bf7c9170d2f5045332b12589832a268a4f4bb0e0
sergio ivan lopez monzon <sergioivan154@gmail.com>
2018-09-10 20:22:55 -0500

    s3 y formulario jalando bien no se esta guardando nada en mysql

--- api/controllers/EstudianteController.js
+++ api/controllers/EstudianteController.js
@@ -10,46 +10,56 @@ module.exports = {
 	},
 	
 	create:function(req, res){
-		var EstudianteObj={
-			nombre:req.param('txtnombre')
-		}
-		Estudiante.create(EstudianteObj,function(err,user){
-			if(err){ 
-				console.log(JSON.stringify(err));
-			    return res.redirect('/estudiantes');
-			}
-				res.redirect('/estudiante');
+
+		// se guarda la imagen en bitbucket
+		const AWS = require('aws-sdk');
+		const BUCKET_NAME = 'pruebasails';
+		const IAM_USER_KEY = 'AKIAJYOH6EKNWFPXMCFQ';
+		const IAM_USER_SECRET = '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';
+		const REGION = 'us-east-2';
+
+		let s3bucket = new AWS.S3({
+		   accessKeyId: IAM_USER_KEY,
+		   secretAccessKey: IAM_USER_SECRET,
+		   Bucket: BUCKET_NAME,
+		   region: REGION
+		 });
+		
+		var params = {
+			    Bucket: BUCKET_NAME,
+			    Key: req.param('txtnombre') + ".jpg",
+			    Body: req.file('avatar'),
+			 };
+
+		s3bucket.upload(params, function (err, data) {
+			
+				//cuando ocurre un problema se informa que no se pudo subir la imagen
+				if (err) {
+				    console.log('No se pudo subir la imagen :(');
+				    console.log(err);
+			    } else{
+
+				    // caso contrario se guarda el estudiante
+					var EstudianteObj={
+						nombre:req.param('txtnombre')
+					}
+					Estudiante.create(EstudianteObj,function(err,user){
+						if(err){ 
+							console.log(JSON.stringify(err));
+						    return res.redirect('/estudiantes');
+						}
+							res.redirect('/estudiante');
+					});
+
+				    console.log('success');
+				    console.log(data);
+				}
 		});
-	},
+			
+		   
+	}
 
-
-
-	uploadFile: function (req, res) {
-
-		const options =
-      	{ // This is the usual stuff
-        	adapter: require('skipper-s3'),
-	      		key: 'AKIAJYOH6EKNWFPXMCFQ',
-		      	secret: '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',
-		      	bucket: 'pruebasails',
-		      	region: 'us-east-2',
-	      	s3params:
-	        	{ 
-	        		ACL: 'public-read'
-	        	},
-      		onProgress: progress => sails.log.verbose('Upload progress:', progress)
-     	}
-
-		req.file('avatar').upload(options, (err, uploadedFiles) => {
-		   if (err) return res.send(500, err);
-				  return res.json({
-				    message: uploadedFiles.length + ' file(s) uploaded successfully!',
-				    files: uploadedFiles
-				  });
-		})
-  	
-  	}
-	
+		
 	
 };
 
